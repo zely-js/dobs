d2dd88583334144ff181e91af1581a3a0aabca13
do4ng <57896501+do4ng@users.noreply.github.com>
2025-11-09 00:32:29 +0900

    feat: add a wrapper function for data caching (#25)

---
+++ packages/dobs/src/experimental/cache.ts
@@ -0,0 +1,130 @@
+import { Routes, Handler } from '../types';
+import { mutateObjectValues } from '../shared/object';
+
+export interface CacheOptions {
+  /**
+   * Time to live (default: 2 * 60 * 1000 ms = 2m)
+   */
+  ttl?: number;
+
+  customCache?: typeof TtlCache;
+}
+
+interface CacheEntry<T> {
+  value: T;
+  expiry: number;
+}
+
+export class TtlCache<K = string, V = any> {
+  private cache: Map<K, CacheEntry<V>> = new Map();
+  private readonly defaultTtl: number;
+
+  constructor(defaultTtl: number = 2 * 6 * 1000) {
+    if (defaultTtl <= 0) {
+      throw new Error('ttl must be more than 0.');
+    }
+    this.defaultTtl = defaultTtl;
+  }
+
+  public set(key: K, value: V, ttl?: number): void {
+    const timeToLive = ttl ?? this.defaultTtl;
+    const expiry = Date.now() + timeToLive;
+    this.cache.set(key, { value, expiry });
+  }
+
+  public get(key: K): V | undefined {
+    const entry = this.cache.get(key);
+
+    if (!entry) {
+      return undefined;
+    }
+
+    if (entry.expiry < Date.now()) {
+      this.cache.delete(key);
+      return undefined;
+    }
+
+    return entry.value;
+  }
+
+  public has(key: K): boolean {
+    return this.get(key) !== undefined;
+  }
+
+  public delete(key: K): boolean {
+    return this.cache.delete(key);
+  }
+
+  public clear(): void {
+    this.cache.clear();
+  }
+
+  public get size(): number {
+    return this.cache.size;
+  }
+}
+
+function normalizePath(path: string) {
+  if (!path) return '/';
+  if (!path.startsWith('/')) path = '/' + path;
+  if (!path.endsWith('/')) path += '/';
+  return path;
+}
+
+function createWrapper(
+  handler: Handler,
+  cache: TtlCache<string, any>,
+  method?: string,
+): Handler {
+  return (req, res) => {
+    const customResponse = { ...res };
+    const defaultSend = res.send;
+
+    const id =
+      normalizePath(req.URL.pathname) + '-' + (method ?? req.method.toUpperCase());
+
+    // has cached data
+    if (cache.has(id)) {
+      return defaultSend(cache.get(id));
+    }
+
+    customResponse.send = (data) => {
+      cache.set(id, data);
+      return defaultSend(data);
+    };
+
+    return handler(req, customResponse as any);
+  };
+}
+
+/**
+ * cache wrapper
+
+ *
+ * @example
+ * ```ts
+ * import { defineRoutes } from "dobs";
+ * import { useCache } from "dobs/experimental";
+ *
+ * export default defineRoutes((req, res) => {}, [useCache()])
+ * ``` 
+ * The values included in the ID generation are as follows: handler ID (GET, ALL, etc.) and pathname.
+ * Values not included are: query, req.body, etc. 
+ */
+export function useCache(cacheOptions?: CacheOptions) {
+  const cache = new (cacheOptions?.customCache ?? TtlCache)(cacheOptions?.ttl);
+
+  // return wrapper
+
+  return (router: Routes): Routes => {
+    const routerType = typeof router === 'function' ? 'function' : 'object';
+
+    if (routerType === 'function') {
+      return createWrapper(router as any, cache, 'all');
+    }
+
+    return mutateObjectValues<string, Handler>(router as any, (value, key) =>
+      createWrapper(value, cache, key),
+    );
+  };
+}
--- packages/dobs/src/shared/object.ts
+++ packages/dobs/src/shared/object.ts
@@ -1,3 +1,19 @@
 export function lowercaseKeyObject(obj) {
   return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k.toLowerCase(), v]));
 }
+
+export function mutateObjectValues<Key = string, Value = any>(
+  obj,
+  fn: (value: Value, key: Key) => any,
+) {
+  if (typeof obj !== 'object' || obj === null) return obj;
+
+  const result: any = Array.isArray(obj) ? [] : {};
+  for (const [key, value] of Object.entries(obj)) {
+    result[key] =
+      typeof value === 'object' && value !== null
+        ? mutateObjectValues(value as Record<string, any>, fn)
+        : fn(value as any, key as any);
+  }
+  return result;
+}
